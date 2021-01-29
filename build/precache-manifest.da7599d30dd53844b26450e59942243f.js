5fbd31b3d1482f7763ada7bb527f8133a2d2a547
Likem Dunyo <likemdunyo@yahoo.com>
2021-01-29 16:54:40 +0000

    Commit build

---
+++ build/precache-manifest.da7599d30dd53844b26450e59942243f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a659d4f17d4971cba22422cfedb949e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bce28890f36f7e18b2e",
+    "url": "/static/css/main.34a48165.chunk.css"
+  },
+  {
+    "revision": "1c98fd786e6b08b0b732",
+    "url": "/static/js/2.7c369be6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7c369be6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9bce28890f36f7e18b2e",
+    "url": "/static/js/main.e8a55f46.chunk.js"
+  },
+  {
+    "revision": "a1c73dd6e874552411e9",
+    "url": "/static/js/runtime-main.c11d9bbc.js"
+  }
+]);
